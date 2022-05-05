49c0786dd4400435bb06c4a2f39ff1e5836be9d9
keys4words <keys4words@gmail.com>
2022-05-05 17:11:32 +0300

    add db

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const { connectDb } = require('./helpers/db');
+const {host, port, db} = require("./configuration");
+const { default: mongoose } = require('mongoose');
 const app = express();
+
+const postSchema = new mongoose.Schema({
+    name: String
+});
+const Post = mongoose.model("Post", postSchema);
+
+
+
+const startServer = ()=>{
+    app.listen(port, ()=>{
+        console.log(`Started API service on port: ${port} at host: ${host}`);
+        console.log(`Our DB: ${db}`);
+        const postOne = new Post({name:"Discovery"});
+        postOne.save(function(err, savedPostOne){
+            if (err) return console.error(err);
+            console.log("savedPostOne", savedPostOne);
+        });
+        // Post.find(function(err, posts){
+        //     if (err) return console.error(err);
+        //     console.log('posts', posts);
+        // });
+    });
+
+};
 
 app.get("/test", (req, res)=>{
     res.send("Our API server is working...");
 });
 
-app.listen(3000, ()=>{
-    console.log("Started API service");
-});
+
+connectDb()
+    .on("error", console.log)
+    .on("disconnected", connectDb)
+    .once("open", startServer);
